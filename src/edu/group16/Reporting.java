e9f7dc24ec58fb2e58ca001ceb7f30941ace0cc9
omradcliffe <radcliffeowe@gmail.com>
2023-02-15 11:08:45 -0500

    Part 3 subpart (0) implemented

---
+++ src/edu/group16/Reporting.java
@@ -0,0 +1,16 @@
+package edu.group16;
+
+public class Reporting {
+
+    public static void main(String[] args) {
+
+        String username = args[0]; //Oracle username
+        String password = args[1]; //Oracle password
+
+        System.out.println("1- Report Patients Basic Information");
+        System.out.println("2- Report Doctors Basic Information");
+        System.out.println("3- Report Admissions Information");
+        System.out.println("4- Update Admissions Payment");
+
+    }
+}
